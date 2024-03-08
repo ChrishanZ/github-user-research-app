592fda23f7c29511e359160add6ff91dfc236969
chrishanz <chrisduttoo@gmail.com>
2024-03-08 19:50:53 +0100

    dev || ready for dynamic calls

--- src/components/Container/Container.jsx
+++ src/components/Container/Container.jsx
@@ -1,8 +1,24 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
 import styles from "./Container.module.scss";
 import SearchBar from "../SearchBar/SearchBar";
 import Profile from "../Profile/Profile";
 
 function Container() {
+  const [data, setData] = useState(null);
+
+  const octokit = new Octokit({
+    auth: "YOUR-TOKEN",
+  });
+
+  useEffect(() => {
+    axios
+      .get()
+      .then((response) => setData(response.data))
+      .catch((error) => console.error(error));
+  }, []);
+
   return (
     <div className={styles.container}>
       <div className={styles.title}>
